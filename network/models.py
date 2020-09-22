6788fd7b8d7968b6e35b5e09b3cd6b0d70152897
LessThanDecent <gabe_guitron@yahoo.com>
2020-09-22 09:58:15 -0700

    Add create post form and functionality

--- network/models.py
+++ network/models.py
@@ -4,3 +4,8 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Post(models.Model):
+    user = models.ForeignKey("User", on_delete=models.CASCADE, related_name="posts")
+    timestamp = models.DateTimeField(auto_now_add=True)
+    content = models.CharField(max_length=255)
--- network/views.py
+++ network/views.py
@@ -1,10 +1,12 @@
+import json
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User
+from .models import User, Post
 
 
 def index(request):
@@ -61,3 +63,18 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "network/register.html")
+
+@login_required
+def post(request):
+    if request.method == "POST":
+    
+        if not request.POST["content"]:
+            return HttpResponseRedirect(reverse('index'))
+        
+        if len(request.POST["content"]) > 255:
+            return HttpResponseRedirect(reverse('index'))
+        
+        post = Post(user=request.user, content=request.POST["content"])
+        post.save()
+
+        return HttpResponseRedirect(reverse('index'))
